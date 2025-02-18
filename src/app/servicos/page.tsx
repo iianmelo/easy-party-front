eda983a8db1b8a240091696b2e0d3d4883754e23
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-02-17 23:28:31 -0300

    ServicosPageDone-Review

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { TopBar, ServiceCard, Footer } from 'components';
+import {
+  PartyHome,
+  DJCard,
+  BuffetCard,
+  PlayCard,
+  PhotoCard,
+  Alimentacao,
+    Decoracao,
+    Midia,
+    Musica,
+    Musica2
+} from 'assets';
+import Image from 'next/image';
+import { Button } from 'components/ui/button';
+
+export default function Servicos() {
+  return (
+    <div className="w-full h-full">
+      <TopBar></TopBar>
+      <div className="flex-col items-center h-full justify-between mb-20">
+        <div className="pl-14 flex flex-col gap-6 w-full text-center justify-start mt-40">
+          <h1 className="text-[56px] font-bold leading-[68px] ">
+            Serviços
+          </h1>
+          <p className="font-normal text-[18px] leading-[27px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="flex justify-center mt-20 mb-20">
+            <input
+            type="text"
+            placeholder="Pesquise serviços para sua festa!"
+            className="border border-black rounded-lg p-2 w-1/2"
+            />
+        </div>
+        <div className="flex gap-5 px-8 h-full w-full justify-center">
+            <div className="flex flex-col justify-between pl-6 pr-6">
+                <Image src={Alimentacao} alt="Logo" className="w-220 h-auto" />    
+            </div>
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-row gap-5">
+                    <div className="justify-center pl-6 pr-6">
+                        <Image src={Musica} alt="Logo" className="w-220 h-auto" />
+                    </div>
+                    <div className="justify-center pl-6 pr-6">
+                        <Image src={Musica2} alt="Logo" className="w-220 h-auto" />
+                    </div>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <div className="justify-center pl-6 pr-6">
+                        <Image src={Decoracao} alt="Logo" className="w-220 h-auto" />
+                    </div>
+                    <div className="justify-center pl-6 pr-6">
+                        <Image src={Midia} alt="Logo" className="w-220 h-auto" />
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+      <Footer></Footer>
+    </div>
+  );
+}
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,5 +5,10 @@ import BuffetCard from './BuffetCard.png';
 import DJCard from './DJCard.png';
 import PlayCard from './PlayCard.png';
 import PhotoCard from './PhotoCard.png';
+import Alimentacao from './Alimentacao.png';
+import Decoracao from './Decoracao.png';
+import Midia from './Midia.png';
+import Musica from './Musica.png';
+import Musica2 from './Musica2.png';
 
-export { Logo, PartyLogo, PartyHome, BuffetCard, DJCard, PlayCard, PhotoCard };
+export { Logo, PartyLogo, PartyHome, BuffetCard, DJCard, PlayCard, PhotoCard, Alimentacao, Decoracao, Midia, Musica, Musica2 };
