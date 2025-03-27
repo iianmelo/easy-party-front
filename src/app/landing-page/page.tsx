8cdb4a2492539d9e8f6cbbd4ea0ad71ae9727aa7
joaofellows <joaovictorfellowsrabelo@gmail.com>
2025-03-26 21:22:44 -0300

    Landing Page done

---
+++ src/app/landing-page/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { TopBar, Footer, PersonalCard } from 'components';
+import {
+  ImgFesta,
+  ImgBalloons,
+  VictorProfile,
+  IanProfile,
+  GuiProfile,
+  RafaProfile,
+  JoaoProfile
+} from 'assets';
+import Image from 'next/image';
+
+export default function Test() {
+  return (
+    <div className="w-full h-full">
+      <TopBar></TopBar>
+      <div className="w-full">
+        <Image src={ImgFesta} alt="Imagem de uma festa" className="h-full w-full" />
+      </div>
+      <div className="flex items-center h-full justify-between">
+        <div className="pl-14 flex flex-col gap-6 max-w-[580px] justify-start">
+          <h1 className="text-[56px] font-bold leading-[68px] ">
+            EasyParty
+          </h1>
+          <p className="font-normal text-[18px] leading-[27px] text-left text-justify">
+            Organizar um evento nunca foi tão fácil! Na Easy Party, transformamos seus momentos especiais em experiências inesquecíveis. Seja um casamento dos sonhos,
+            uma festa de aniversário, um evento corporativo ou qualquer outra celebração, cuidamos de cada detalhe para que você aproveite ao máximo. Oferecemos buffet completo,
+            decoração personalizada, música, iluminação, entretenimento e muito mais, tudo pensado para atender ao seu estilo e necessidade. Com uma equipe experiente e serviços
+            de alta qualidade, garantimos eventos únicos, cheios de sabor e sofisticação.
+          </p>
+        </div>
+        <Image src={ImgBalloons} alt="Logo" className='mr-20' />
+      </div>
+      <div className="flex flex-col py-10 gap-10 w-full bg-[#D53232]">
+        <div className="flex justify-between px-16 py-5 items-center">
+          <div className="flex flex-col gap-4">
+            <p className="font-bold text-[18px] leading-[58px]">
+                Nosso time
+            </p>
+            <h1 className="font-bold text-[48px] leading-[58px]">
+              {' '}
+              Pessoas resposáveis por desenvolver a plataforma
+            </h1>
+            <p className="font-normal text-[18px] leading-[27px]">
+                O nosso time é formado por jovens empreendedores que pensam e colocam em prática
+                soluções para problemas do seu dia a dia.
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-14 px-8 h-full justify-self-center">
+          <PersonalCard
+            image={RafaProfile}
+            name="Rafael Alves"
+            ocupation="Desenvolvedor de Software" 
+          />
+          <PersonalCard
+            image={JoaoProfile}
+            name="João Fellows"
+            ocupation="Desenvolvedor de Software"
+          />
+          <PersonalCard
+            image={GuiProfile}
+            name="Guilherme Montenegro"
+            ocupation="Desenvolvedor de Software"
+          />
+          <PersonalCard
+            image={VictorProfile}
+            name="Victor Mendonça"
+            ocupation="Desenvolvedor de Software"
+          />
+          <PersonalCard
+            image={IanProfile}
+            name="Ian Melo"
+            ocupation="Desenvolvedor de Software"
+          />
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import Location from './location';
 import ServiceCard from './service-card';
 import Footer from './footer';
 import TopBarLogged from './top-bar-logged';
+import PersonalCard from './personal-card';
 
-export { TopBar, Location, ServiceCard, Footer, TopBarLogged };
+export { TopBar, Location, ServiceCard, Footer, TopBarLogged, PersonalCard };
